268f5a80db1c6165db4735376d260048cafd9889
Sanzhar <sanzhar42@gmail.com>
2021-06-16 23:15:05 +0600

    fix: remove snap scroll

--- src/components/Landing/styled.ts
+++ src/components/Landing/styled.ts
@@ -2,17 +2,9 @@ import styled from 'styled-components';
 
 export const LandingContainer = styled.div`
   width: 100%;
-  /* overflow: auto;
-  height: 100vh; */
 
   position: relative;
 
   display: flex;
   flex-flow: column nowrap;
-  /* scroll-snap-type: mandatory;
-  scroll-snap-points-y: repeat(3rem);
-  scroll-snap-type: y mandatory; */
-  .section {
-    scroll-snap-align: start;
-  }
 `;
